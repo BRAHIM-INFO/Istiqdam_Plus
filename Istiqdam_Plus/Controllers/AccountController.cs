190d6d90615da7c4ebfc85ec25db3a648c34ba15
IBRAHIM BENYOUNES <benyounesibrahim@hotmail.com>
2026-06-07 15:33:33 +0100

    First_Day

---
+++ Istiqdam_Plus/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Istiqdam_Plus.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View(); // يعرض صفحة تسجيل الدخول
+        }
+    }
+}
--- Istiqdam_Plus/Controllers/HomeController.cs
+++ Istiqdam_Plus/Controllers/HomeController.cs
@@ -37,6 +37,12 @@ namespace Istiqdam_Plus.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
     }
 }
 
